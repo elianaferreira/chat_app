0cd44211767d053a944c8b190bee3f95675c5f9e
Eliana Ferreira <ebferreiran@gmail.com>
2023-07-09 15:07:58 -0400

    added logic to display the sent messages

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:chat_app/utils/constants.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No mesages found'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection(Constants.firestoreChatsCollectionName)
+          .orderBy(UserMapKeys.createdAt, descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(child: Text('No messages found.'));
+        }
+        if (snapshot.hasError) {
+          return const Center(
+              child: Text(
+            'Something went wrong.',
+            style: TextStyle(color: Colors.red),
+          ));
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) =>
+              Text(loadedMessages[index].data()[UserMapKeys.text]),
+        );
+      },
     );
   }
 }
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,8 +1,9 @@
-import 'package:chat_app/utils/constants.dart';
-import 'package:chat_app/utils/dimens.dart';
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+
+import 'package:chat_app/utils/constants.dart';
+import 'package:chat_app/utils/dimens.dart';
 
 class NewMessage extends StatefulWidget {
   const NewMessage({super.key});
