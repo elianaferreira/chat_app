7f3fce1174a7bab170156eb253c348a847378756
Eliana Ferreira <ebferreiran@gmail.com>
2023-07-08 21:14:30 -0400

    added pending firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:chat_app/screens/auth.dart';
 
@@ -9,7 +11,11 @@ final theme = ThemeData().copyWith(
   colorScheme: colorScheme,
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
